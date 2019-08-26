fefe145a0d76d42473a68beb357a3caa0f6d3361
ndurbin <nick.durbin@gmail.com>
2019-08-26 08:37:02 -0400

    Removed boiler plate. Got the app setup with CRA.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
