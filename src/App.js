3bb12c4a8596c60bee0094cf79dfa8339eb8e3b0
ndurbin <nick.durbin@gmail.com>
2019-08-26 08:58:42 -0400

    Added some semantic JSX to the Home, imported in our data, and created out avenger cards.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-
+import Home from './Componenets/Home';
+import AvengersList from './Components/AvengersList';
+import avengers from '../Data';
 
 function App() {
+
+  const [avenger, setAvenger] = useState();
+
   return (
     <div className="App">
-      
+      <Route exact path='/' component={Home} />
+      <Route exact path='/avengers' component={AvengerList} />
     </div>
   );
 }
---
+++ src/Components/AvengersList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import avengers from '../../Data';
+
+function AvengersList() {
+  return (
+    <div className="characters-list-wrapper">
+      {avengers.map(avenger => (
+        <div className="character-card" key={avenger.id}>
+          <img src={avenger.thumbnail} alt={avenger.name} />
+          <h2>{avenger.name}</h2>
+          <p>({avenger.nickname})</p>
+        </div>
+      ))};
+    </div>
+  )
+}
+
+export default AvengersList;
